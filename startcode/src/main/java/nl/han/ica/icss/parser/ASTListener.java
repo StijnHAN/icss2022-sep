f2c48797c9819606210aaeb4a0fb089425593712
Stijn de Jong <s.dejong15@student.han.nl>
2025-06-07 18:32:59 +0200

    Level 0 Parser

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -1,28 +1,532 @@
 package nl.han.ica.icss.parser;
 
-
 import nl.han.ica.datastructures.HANStack;
 import nl.han.ica.datastructures.IHANStack;
 import nl.han.ica.icss.antlr.ICSSBaseListener;
+import nl.han.ica.icss.antlr.ICSSParser;
 import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.ColorLiteral;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
+import nl.han.ica.icss.ast.selectors.ClassSelector;
+import nl.han.ica.icss.ast.selectors.IdSelector;
+import nl.han.ica.icss.ast.selectors.TagSelector;
+import org.antlr.v4.runtime.ParserRuleContext;
 
 /**
  * This class extracts the ICSS Abstract Syntax Tree from the Antlr Parse tree.
  */
 public class ASTListener extends ICSSBaseListener {
-	
-	//Accumulator attributes:
-	private AST ast;
 
-	//Use this to keep track of the parent nodes when recursively traversing the ast
-	private IHANStack<ASTNode> currentContainer;
+    //Accumulator attributes:
+    private AST ast;
 
-	public ASTListener() {
-		ast = new AST();
-		currentContainer = new HANStack<>();
-	}
+    //Use this to keep track of the parent nodes when recursively traversing the ast
+    private IHANStack<ASTNode> currentContainer;
+
+    public ASTListener() {
+        ast = new AST();
+        currentContainer = new HANStack<>();
+    }
+
     public AST getAST() {
         return ast;
     }
-    
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterStylesheet(ICSSParser.StylesheetContext ctx) {
+        ASTNode stylesheet = new Stylesheet();
+        currentContainer.push(stylesheet);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitStylesheet(ICSSParser.StylesheetContext ctx) {
+        ASTNode stylesheet = currentContainer.pop();
+        ast.setRoot((Stylesheet) stylesheet);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterStyleRule(ICSSParser.StyleRuleContext ctx) {
+        ASTNode styleRule = new Stylerule();
+        currentContainer.push(styleRule);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitStyleRule(ICSSParser.StyleRuleContext ctx) {
+        ASTNode styleRule = currentContainer.pop();
+        currentContainer.peek().addChild(styleRule);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterClassSelector(ICSSParser.ClassSelectorContext ctx) {
+        ASTNode classSelector = new ClassSelector(ctx.CLASS_IDENT().getText());
+        currentContainer.push(classSelector);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitClassSelector(ICSSParser.ClassSelectorContext ctx) {
+        ASTNode classSelector = currentContainer.pop();
+        currentContainer.peek().addChild(classSelector);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterIdSelector(ICSSParser.IdSelectorContext ctx) {
+        ASTNode idSelector = new IdSelector(ctx.ID_IDENT().getText());
+        currentContainer.push(idSelector);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitIdSelector(ICSSParser.IdSelectorContext ctx) {
+        ASTNode idSelector = currentContainer.pop();
+        currentContainer.peek().addChild(idSelector);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterTagSelector(ICSSParser.TagSelectorContext ctx) {
+        ASTNode tagSelector = new TagSelector(ctx.LOWER_IDENT().getText());
+        currentContainer.push(tagSelector);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitTagSelector(ICSSParser.TagSelectorContext ctx) {
+        ASTNode tagSelector = currentContainer.pop();
+        currentContainer.peek().addChild(tagSelector);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterDeclaration(ICSSParser.DeclarationContext ctx) {
+        ASTNode declaration = new Declaration();
+        currentContainer.push(declaration);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitDeclaration(ICSSParser.DeclarationContext ctx) {
+        ASTNode declaration = currentContainer.pop();
+        currentContainer.peek().addChild(declaration);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterPropertyName(ICSSParser.PropertyNameContext ctx) {
+        ASTNode propertyName = new PropertyName(ctx.getText());
+        currentContainer.push(propertyName);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitPropertyName(ICSSParser.PropertyNameContext ctx) {
+        ASTNode propertyName = currentContainer.pop();
+        currentContainer.peek().addChild(propertyName);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterBoolLiteral(ICSSParser.BoolLiteralContext ctx) {
+        super.enterBoolLiteral(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitBoolLiteral(ICSSParser.BoolLiteralContext ctx) {
+        super.exitBoolLiteral(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterColorLiteral(ICSSParser.ColorLiteralContext ctx) {
+        ASTNode colorLiteral = new ColorLiteral(ctx.COLOR().getText());
+        currentContainer.push(colorLiteral);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitColorLiteral(ICSSParser.ColorLiteralContext ctx) {
+        ASTNode colorLiteral = currentContainer.pop();
+        currentContainer.peek().addChild(colorLiteral);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterPercentageLiteral(ICSSParser.PercentageLiteralContext ctx) {
+        super.enterPercentageLiteral(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitPercentageLiteral(ICSSParser.PercentageLiteralContext ctx) {
+        super.exitPercentageLiteral(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterPixelLiteral(ICSSParser.PixelLiteralContext ctx) {
+        ASTNode pixelLiteral = new PixelLiteral(ctx.PIXELSIZE().getText());
+        currentContainer.push(pixelLiteral);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitPixelLiteral(ICSSParser.PixelLiteralContext ctx) {
+        ASTNode pixelLiteral = currentContainer.pop();
+        currentContainer.peek().addChild(pixelLiteral);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterScalarLiteral(ICSSParser.ScalarLiteralContext ctx) {
+        super.enterScalarLiteral(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitScalarLiteral(ICSSParser.ScalarLiteralContext ctx) {
+        super.exitScalarLiteral(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterVariableAssignment(ICSSParser.VariableAssignmentContext ctx) {
+        super.enterVariableAssignment(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitVariableAssignment(ICSSParser.VariableAssignmentContext ctx) {
+        super.exitVariableAssignment(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterVariableReference(ICSSParser.VariableReferenceContext ctx) {
+        super.enterVariableReference(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitVariableReference(ICSSParser.VariableReferenceContext ctx) {
+        super.exitVariableReference(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterIfClause(ICSSParser.IfClauseContext ctx) {
+        super.enterIfClause(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitIfClause(ICSSParser.IfClauseContext ctx) {
+        super.exitIfClause(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterElseClause(ICSSParser.ElseClauseContext ctx) {
+        super.enterElseClause(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitElseClause(ICSSParser.ElseClauseContext ctx) {
+        super.exitElseClause(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterAddOperation(ICSSParser.AddOperationContext ctx) {
+        super.enterAddOperation(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitAddOperation(ICSSParser.AddOperationContext ctx) {
+        super.exitAddOperation(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterMultiplyOperation(ICSSParser.MultiplyOperationContext ctx) {
+        super.enterMultiplyOperation(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitMultiplyOperation(ICSSParser.MultiplyOperationContext ctx) {
+        super.exitMultiplyOperation(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterSubtractOperation(ICSSParser.SubtractOperationContext ctx) {
+        super.enterSubtractOperation(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitSubtractOperation(ICSSParser.SubtractOperationContext ctx) {
+        super.exitSubtractOperation(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void enterEveryRule(ParserRuleContext ctx) {
+        super.enterEveryRule(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public void exitEveryRule(ParserRuleContext ctx) {
+        super.exitEveryRule(ctx);
+    }
 }
